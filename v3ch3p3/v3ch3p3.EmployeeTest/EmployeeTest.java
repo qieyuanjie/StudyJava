7599eed43264ecc29d112354506c7fabe3d46c36
HYe2cS <45024266+HYe2cS@users.noreply.github.com>
2020-10-04 11:21:46 +0800

    upload new test and demo

--- v3ch3p3/v3ch3p3.EmployeeTest/EmployeeTest.java
+++ v3ch3p3/v3ch3p3.EmployeeTest/EmployeeTest.java
@@ -8,21 +8,28 @@ import java.time.*;
  */
 public class EmployeeTest {
     public static void main(String[] args) {
-        // fill the staff array with three Employee objects
+        //构造一个Employee数组，并且填入3个Employee对象
         Employee[] staff = new Employee[3];
 
         staff[0] = new Employee("Carl Cracker", 75000, 1987, 12, 15);
         staff[1] = new Employee("Harry Hacker", 50000, 1989, 10, 1);
         staff[2] = new Employee("Tony Tester", 40000, 1990, 3, 15);
 
-        // raise everyone's salary by 5%
-        for (Employee e : staff)
+        //使用raiseSalary方法提高每个员工的薪水5%
+        for (Employee e : staff
+        ) {
             e.raiseSalary(5);
 
-        // print out information about all Employee objects
-        for (Employee e : staff)
-            System.out.println("name=" + e.getName() + ",salary=" + e.getSalary() + ",hireDay="
-                    + e.getHireDay());
+        }
+
+        //调用getName方法，getSalary方法和getHireDay方法打印各个员工的信息
+        for (Employee e : staff
+        ) {
+            System.out.println("姓名：" + e.getName() + ","
+                    + "工资" + e.getSalary() + ","
+                    + "HireDay" + e.getHireDay());
+
+        }
     }
 }
 
@@ -33,12 +40,12 @@ class Employee {
     private double salary;
 
     //构造器
-    public Employee(String n,double s,int yesr,int month,int day){
+
+    public Employee(String n, double s, int year, int month, int day) {
         name = n;
         salary = s;
-        hireDay = LocalDate.of(yesr,month,day);
+        hireDay = LocalDate.of(year, month, day);
     }
-
 
     public String getName() {
         return name;
